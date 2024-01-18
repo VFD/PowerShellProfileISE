8a043a29a597e1078caf2beeb4e471fcd8c37f9c
VFD <4848759+VFD@users.noreply.github.com>
2024-01-18 11:10:25 +0100

    Add files via upload

---
+++ Microsoft.PowerShellISE_profile.ps1
@@ -0,0 +1,22 @@
+<#
+.SYNOPSIS
+Profile for the PowerShell ISE
+#>
+
+
+# to simplify Access
+$MyPowerShell = "$(split-path $profile)"
+$MyScripts = "$(split-path $profile)\Scripts"
+$MyScriptsDev = "$(split-path $profile)\ScriptsDev"
+$MyModules = "$(split-path $profile)\Modules"
+$MyDocs    =  [environment]::GetFolderPath('MyDocuments')
+$MyDesktop =  [environment]::GetFolderPath('Desktop')
+$MyProjects = "$MyDocs\Projects"
+$MyTemp = "C:\Temp"
+
+
+# No need to Add path for modules
+# Only for Scripts, at the beginning
+$env:Path = $MyScripts + ";" + $env:Path
+
+# -----[EOP]------------------------------------------------------------
