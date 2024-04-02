e3dfce8fd2c1707e88754cba6fbf836786713e9c
VFD <vincent.f.dubois@outlook.fr>
2024-04-02 16:39:57 +0200

    Update Microsoft.PowerShellISE_profile.ps1

--- Microsoft.PowerShellISE_profile.ps1
+++ Microsoft.PowerShellISE_profile.ps1
@@ -3,6 +3,24 @@
 Profile for the PowerShell ISE
 #>
 
+function prompt {
+	Write-Host
+	Write-Host -NoNewLine "["
+	Write-Host -NoNewLine (Get-Date -Format "dd/MM/yyyy | HH:mm") -ForegroundColor White
+	Write-Host -NoNewLine "] "
+	Write-Host -NoNewLine $env:UserName.ToLower()
+	Write-Host -NoNewLine "@"
+	Write-Host -NoNewLine $env:UserDomain.ToLower()
+	Write-Host -NoNewLine " on "
+	Write-Host -NoNewLine $env:ComputerName
+	Write-Host -NoNewLine " ("
+	Write-Host -NoNewLine $env:OS
+	Write-Host -NoNewLine ")"
+	Write-Host -NoNewLine "`n[> "
+	Write-Host -NoNewLine (Get-Location).Path -ForegroundColor Red
+	Write-Host -NoNewLine " <]"
+	return "`nPS > "
+}
 
 # to simplify Access
 $MyPowerShell = "$(split-path $profile)"
